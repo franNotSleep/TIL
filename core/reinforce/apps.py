86da188ff9951b0b15463d21f54257900d5353bb
Francisco Jose <lopezozunafranciscojose17@gmail.com>
2023-04-06 10:23:38 +0800

    Backend set up

---
+++ core/reinforce/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ReinforceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core.reinforce"
+    label = "core_reinforce"
