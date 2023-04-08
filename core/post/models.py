5a563a2ea692399268a0d6c0dd9a581ccdc7b32b
Francisco Jose <lopezozunafranciscojose17@gmail.com>
2023-04-08 09:29:23 +0800

    Create post model

--- core/post/models.py
+++ core/post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from core.abstract.models import AbstractManager, AbstractModel
 
-# Create your models here.
+
+class PostManager(AbstractManager):
+    pass
+
+
+class Post(AbstractModel):
+    author = models.ForeignKey(to="core_user.User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+
+    objects = PostManager()
+
+    def __str__(self):
+        return f"{self.title}"
